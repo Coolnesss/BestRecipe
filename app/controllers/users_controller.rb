8501e51f2f1ad11fd49e0ac042ea200933b29d52
Juho Kostet <juho.kostet@cs.helsinki.fi>
2015-03-11 23:43:18 +0200

    Allow creating new users without signing in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-  before_action :ensure_that_signed_in, except: [:index, :show, :new]
+  before_action :ensure_that_signed_in, except: [:index, :show, :new, :create]
 
   # GET /users
   # GET /users.json
