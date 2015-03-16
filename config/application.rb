bc903c3813ab27dbdaadc74ad0e8c7291af05add
Chang <chang.rajani@gmail.com>
2015-03-17 00:28:53 +0200

    Added AngularJS search function for ingredients on new recipe page.

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module BestRecipe
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.serve_static_files = true
+    config.assets.debug = false 
   end
 end
