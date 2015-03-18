d813aca91aaed12026846cf7d12d0329477c5a3f
Chang <chang.rajani@gmail.com>
2015-03-18 15:40:33 +0200

    Fixed line spacings in descriptions. Added ingredient listing in recipe SHOW. Added massCreate method which can create multiple recipe_ingredients for future clickable list

--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -1,4 +1,9 @@
 class RecipeIngredient < ActiveRecord::Base
   belongs_to :ingredient
   belongs_to :recipe
+
+  def self.massCreate(args)
+    args.each{|z, y| RecipeIngredient.create(recipe_id:z, ingredient_id:y)}
+  end
+
 end
